d0d959e2843dec4a2442d1f2c40efc2bc278dd20
Vse__Putyom <65615327+VsePutym@users.noreply.github.com>
2024-08-18 15:46:58 +0300

    Создание проекта

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Мой новый проект</h1>
+      <p>Мой новый проект разработка Библиотеки фильмов на React RTK TS</p>
+      <p>Trakt.tv гланый API</p>
+    </div>
+  );
+}
+
+export default App;
